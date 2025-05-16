3b17976b09d4637cf519187275ffc45ba0a692ab
theonebaahir <babusadudekula@gmail.com>
2025-05-16 21:12:28 +0530

    Add files via upload

---
+++ usawgv.py
@@ -0,0 +1,37 @@
+import turtle
+turtle.Screen().bgcolor("Aqua")
+board = turtle.Turtle()
+
+board.forward(100)
+
+
+board.left(120)
+board.forward(100)
+
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+
+
+board.pendown()
+
+board.right(124)
+board.forward(1456)
+
+
+board.right(120)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+turtle.done()
